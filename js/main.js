0c677640896efd1fdffdeb7fa4e35a8ddf9d0b07
thalaczkiewicz <halaczkiewiczt@gmail.com>
2017-06-05 16:00:39 +0200

    obj

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
+var obj={};
+
 var preperePost = function( json ) {
-    html = [];
+    var html = [];
     var promise = $.Deferred();
     for (i = 0; i < json.length; i++) {
         html[i] = Mustache.to_html(template, json[i]);
     }
-    promise.resolve();
+    obj.html=html;
+    promise.resolve(html);
     return promise;
 };
 
-var getPosts = function () {
+var getPosts = function (html) {
     var promise = $.Deferred();
     for(i=0; i < 10; i++) {
         $('#wall').append(html.pop());
@@ -20,10 +23,21 @@ $.getJSON( "http://jsonplaceholder.typicode.com/posts" ).then(preperePost
 ).then(getPosts);
 
 
-$(window).on('scroll', function() {
+
+obj.Next = function() {
+    //this//obj;
     if ($(window).scrollTop() >= $('#wall').offset().top + $('#wall').outerHeight() - window.innerHeight) {
-        getPosts(html);
+        getPosts(obj.html);
     }
-});
+}
+
+
+$(window).on('scroll', obj.Next
+);
+
+console.log(obj.Next);
+console.log(obj.Next);
+console.log(obj);
+
 
 
