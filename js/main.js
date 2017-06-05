deec5f8e0f2045742efaf81f8086bc364f938dac
thalaczkiewicz <halaczkiewiczt@gmail.com>
2017-06-05 14:06:52 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,39 +1,31 @@
-var template = "<div class='card mb-3 text-center'><div class='card-block '><blockquote class='card-blockquote'><p><b>{{title}}</b></p> <footer>{{body}}</footer></blockquote></div></div>";
-var html = [];
-var counter = 0;
-// function getPosts() {
-//     var counter = 0;
-//     if (counter < html.length) {
-//         for (var i = 0; i < 10; i++) {
-//             $('#wall').append(html[counter]);
-//             counter += 1;
-//             console.log(html[counter]);
-//         }
-//     }
-// }
-var getPosts = function () {
-    for(i=0; i < 10; i++) {
-        $('#wall').append(html.pop());
-    }
-}
+
 
 
 var preperePost = function( json ) {
+    var html = [];
+    var promise = $.Deferred();
+
     for (i = 0; i < json.length; i++) {
         html[i] = Mustache.to_html(template, json[i]);
     }
+    promise.resolve(html);
+    return promise;
+};
+
+var getPosts = function (html) {
+    var promise = $.Deferred();
+    for(i=0; i < 10; i++) {
+        $('#wall').append(html.pop());
+    }
+    promise.resolve(html);
+    return promise;
 };
 
 
-$.getJSON( "http://jsonplaceholder.typicode.com/posts" ).done(function (args) {
-    preperePost(args);
-}).done(function () {
-    for(i=0; i < 10; i++) {
-        getPosts();
-    }
-});
+$.getJSON( "http://jsonplaceholder.typicode.com/posts" ).then(preperePost
+).then(getPosts);
 
-$(window).bind('scroll', function() {
+$(window).on('scroll', function() {
     if ($(window).scrollTop() >= $('#wall').offset().top + $('#wall').outerHeight() - window.innerHeight) {
         getPosts();
     }
---
+++ js/template.js
@@ -0,0 +1,1 @@
+var template = "<div class='card mb-3 text-center'><div class='card-block '><blockquote class='card-blockquote'><p><b>{{title}}</b></p> <footer>{{body}}</footer></blockquote></div></div>";
