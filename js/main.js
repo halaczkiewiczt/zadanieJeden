d5bef2875d3a0fc30b910706795e8bc7213b1be6
thalaczkiewicz <halaczkiewiczt@gmail.com>
2017-06-05 15:20:27 +0200

    changed var html to global

--- js/main.js
+++ js/main.js
@@ -1,33 +1,28 @@
-
-
-
 var preperePost = function( json ) {
-    var html = [];
+    html = [];
     var promise = $.Deferred();
-
     for (i = 0; i < json.length; i++) {
         html[i] = Mustache.to_html(template, json[i]);
     }
-    promise.resolve(html);
+    promise.resolve();
     return promise;
 };
 
-var getPosts = function (html) {
+var getPosts = function () {
     var promise = $.Deferred();
     for(i=0; i < 10; i++) {
         $('#wall').append(html.pop());
     }
-    promise.resolve(html);
-    return promise;
+    promise.resolve();
 };
-
 
 $.getJSON( "http://jsonplaceholder.typicode.com/posts" ).then(preperePost
 ).then(getPosts);
 
+
 $(window).on('scroll', function() {
     if ($(window).scrollTop() >= $('#wall').offset().top + $('#wall').outerHeight() - window.innerHeight) {
-        getPosts();
+        getPosts(html);
     }
 });
 
